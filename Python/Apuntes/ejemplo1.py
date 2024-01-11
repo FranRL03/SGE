573f2971c99ac3dbcad83160f74a400d7d431e1a
Fran Ruiz <fjavierrule@gmail.com>
2024-01-11 13:31:42 +0100

    python

---
+++ Python/Apuntes/ejemplo1.py
@@ -0,0 +1,20 @@
+palabra = "Python"
+print(palabra[0:2])
+print(str(5))
+
+lista1 = [1,2,3,4,5]
+lista2 = list([1,2,3,4,5])
+
+print(lista1[-2:])
+
+listaMixta = ["Pi", 3.14, None, -15]
+
+print(lista1 + listaMixta)
+
+n = 1
+
+if(n > 0):
+    print("Mayor")
+else:
+    print("menor")
+    print(n)
