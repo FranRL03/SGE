0514116c08b7d1b0cca299944b432b3aad21a91c
Fran Ruiz <fjavierrule@gmail.com>
2024-01-12 13:43:29 +0100

    terminar el ejercicio de python

---
+++ Python/Boletin1/ejercicio_1.py
@@ -0,0 +1,9 @@
+
+print("==================")
+print("[1] Importar palabras clave")
+print("[2] Mostrar palabras clave")
+print("[0] Salir")
+print("==================")
+
+print("Introduzca un número")
+op = input()
